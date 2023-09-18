4e8a7cefe873f464c1a2d305e94a02c6e489d9ef
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-09-18 14:25:48 +0000

    Add insructions

--- run.py
+++ run.py
@@ -12,8 +12,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('psychological_test')
 
-questions = SHEET.worksheet('questions')
+# questions = SHEET.worksheet('questions')
 
-data = questions.get_all_values()
+# data = questions.get_all_values()
 
-pprint(data)
+# print(data)
+
+def info():
+    """
+    Information about the purpose of psychological 
+    testing and instructions for taking the test
+    """
+    print("""\n Welcome to psychological testing. By passing the temperament test,
+you will be able to better know your own Self. You will understand what
+your character is like and will be able to take a more correct position in life.
+Knowing the temperament of your loved ones and friends will help you get along comfortably
+in the family and in the work team.\n""")
+    print("Instructions:\n")
+    print("""  You are asked to answer 57 questions. The questions are aimed at identifying your usual way of behavior. 
+Try to imagine typical situations and give the first “natural” answer that comes to mind. If you agree with the statement, 
+indicate 'yes', if not, indicate 'no'.\n""")
+
+info()   
