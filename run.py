2ddbfc4012ac5c0f36bac834757d51d42aa5e365
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-09-18 16:20:58 +0000

    Add user request loop

--- run.py
+++ run.py
@@ -18,40 +18,46 @@ SHEET = GSPREAD_CLIENT.open('psychological_test')
 
 # print(data)
 
+
 def info():
     """
     Information about the purpose of psychological 
     testing and instructions for taking the test
     """
-    print("""\n Welcome to psychological testing. By passing the temperament test,
-you will be able to better know your own Self. You will understand what
-your character is like and will be able to take a more correct position in life.
-Knowing the temperament of your loved ones and friends will help you get along comfortably
-in the family and in the work team.\n""")
-
-    print("Instructions:\n")
-
-    print("""  You are asked to answer 57 questions. The questions are aimed at identifying 
-your usual way of behavior. Try to imagine typical situations and give the first “natural” 
-answer that comes to mind. If you agree with the statement, indicate 'yes', if not, indicate 'no'.\n""")
-
-    data_name = input("Enter your name: ")
-    validate_data(data_name)
     
+    print("""\n Welcome to psychological testing. By passing the temperament test,
+    you will be able to better know your own Self. You will understand what
+    your character is like and will be able to take a more correct position in life.
+    Knowing the temperament of your loved ones and friends will help you get along comfortably
+    in the family and in the work team.\n""")
+    while True:
+        data_name = input("Enter your name: ")
+
+        if validate_data(data_name):
+            print("Data is valid")
+            break
+    
+
 def validate_data(values):
     """
     This function checks the correctness of the data entered by the user
     """
     try:
-        if len(values) > 10:
+        if len(values) < 3 or len(values) > 10:
             raise ValueError(
-                f"the length of your name should not exceed 10 characters. You entered: {len(values)}characters"
+                f"the length of your name should not be less than 3 characters and not exceed 10 characters. You entered: {len(values)}characters"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, try again.\n")
+        return False
+    
+        print("\nInstructions:\n")
 
-    print(values)
-
+        print(f"""  {values}, you are asked to answer 57 questions. The questions are aimed at identifying 
+    your usual way of behavior. Try to imagine typical situations and give the first “natural” 
+    answer that comes to mind. If you agree with the statement, indicate 'yes', if not, indicate 'no'.\n""")
+        
+    return True   
 
 
 
