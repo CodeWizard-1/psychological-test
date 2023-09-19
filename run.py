9cf37db5fec4c710e21966538e655953f66fc80b
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-09-19 13:19:34 +0000

    Add: highlighted important text in green and blue

--- run.py
+++ run.py
@@ -12,6 +12,10 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('psychological_test')
 
+RED = '\033[91m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+RESET = '\033[0m'
 
 # questions = SHEET.worksheet('questions')
 
@@ -43,14 +47,14 @@ def validate_data(values):
     try:
         if len(values) < 3 or len(values) > 10:
             raise ValueError(
-                f"the length of your name should not be less than 3 characters and not exceed 10 characters. You entered: {len(values)}characters"
+                f"{RED}the length of your name should not be less than 3 characters and not exceed 10 characters. You entered: {len(values)} characters,{RESET}"
             )
     except ValueError as e:
-        print(f"Invalid data: {e}, try again.\n")
+        print(f"{RED}Invalid data: {e} {RED}try again.{RESET}\n")
         return False
     
     print("\nInstructions:\n")
-    print(f"""  {values}, you are asked to answer 57 questions. The questions are aimed at identifying your usual way of behavior. Try to imagine typical situations and give the first “natural” answer that comes to mind. If you agree with the statement, indicate 'Yes', if not, indicate 'No'.\n""")
+    print(f"""  {GREEN}{values}{RESET}, you are asked to answer 57 questions. The questions are aimed at identifying your usual way of behavior. Try to imagine typical situations and give the first “natural” answer that comes to mind. If you agree with the statement, indicate 'Yes', if not, indicate 'No'.\n""")
         
     return True   
 
@@ -73,9 +77,9 @@ resalts_scale_lies = 0
 
 
 for idx, question in enumerate (questions, start = 1):
-    answer = input(f"\nQuestion № {idx} : {question}(Enter 'Y' or 'N')")
+    answer = input(f"\n{BLUE}Question № {idx}{RESET} : {question} ({GREEN}Enter 'Y' or 'N'){RESET}")
     while answer.lower() not in ('y','n'):
-        answer = input("\nPlease, enter 'Y or 'N'")
+        answer = input(f"\n{RED}Please, enter 'Y or 'N'{RESET}")
     if idx in target_question_extra_intro_yes and answer.lower() == 'y':
         resalts_extra_intro += 1
     if idx in target_question_extra_intro_no and answer.lower() == 'n':
@@ -89,7 +93,7 @@ for idx, question in enumerate (questions, start = 1):
 
 
 
-print("\nThanks for the answers, no more questions\n")
+print(f"\n{BLUE}Thanks for the answers, no more questions{RESET}\n")
  
 if resalts_extra_intro <= 12 and resalts_neuroticism <= 12:
     temperament_type = 'Phlegmatic'
@@ -127,8 +131,8 @@ if resalts_extra_intro <= 12:
 
 
 if resalts_scale_lies >= 4:
-    massage = f"""<span style='color: red;'>Important! {data_name}, you answered not as you really are, but as you would like or as accepted in society. In other words, your answers are not reliable.</span>\n"""
-    print(massage)
+    print(f"{RED}Important! {data_name}, you answered not as you really are, but as you would like or as accepted in society. In other words, your answers are not reliable.{RESET}\n")
+    
 
 result_data = [
     ["Name", "Extra-Introversion Points", "Neuroticism Points", "Scale Lies Points", "Temperament Type"],
