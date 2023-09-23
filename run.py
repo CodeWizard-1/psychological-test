e5b01c26070423f09e526c029035637a32614d29
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-09-23 21:18:29 +0000

    Update all text

--- run.py
+++ run.py
@@ -33,8 +33,7 @@ RESET = '\033[0m'
 # This is an ASCII-art representation that is printed to the console
 # at the beginning of your script for decorative purposes
 
-#art = 
-"""
+art = """
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠈⠻⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠟⠁⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⠀⠀⠀⠀⢸⣿⣿⣿⣷⣄⠀⠀⠀⠀⣠⣾⣿⣿⣿⡇⠀⠀⠀⠀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⢸⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⣿⡇⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
@@ -52,9 +51,9 @@ RESET = '\033[0m'
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 
 """
-# for sign in art:
-#     print(sign, end='', flush=True)
-#     time.sleep(0.005)
+for sign in art:
+    print(sign, end='', flush=True)
+    time.sleep(0.005)
 
 
 
@@ -62,21 +61,20 @@ def menu():
     """
     Display a menu and prompt the user for their choice.
     """
-    # data_name = info()
     while True:
-        print("\nMenu:")
-        print("1. Start the quiz")
-        print("2. Check your previous score")
-        print("3. Quit")
+        print("\nMenu:\n")
+        print("1. Start psychological testing")
+        print("2. View previous test results")
+        print("3. Exit the program")
         
-        choice = input("Enter the number of your choice: ")
+        choice = input("\nEnter the number of your choice: ")
         
         if choice == "1":
             info()
         elif choice == "2":
             check_previous_score()
         elif choice == "3":
-            print("\n Thank you for participating in our test!")
+            print("\n Thank you for your time. You can take the test at any time convenient for you. See you again!!")
             break
         else:
             print("Invalid choice. Please enter 1, 2, or 3.")
@@ -87,7 +85,7 @@ def check_previous_score():
     Check the user's previous score by entering their name.
     """
     existing_sheets = [worksheet.title for worksheet in SHEET.worksheets()]
-    data_name = input(f"{BLUE}\nEnter your name to check your previous score:{RESET}\n")
+    data_name = input(f"{BLUE}\nEnter your name to get your previous test results: {RESET}\n")
 
     if data_name in existing_sheets:
         worksheet = SHEET.worksheet(data_name)
@@ -99,15 +97,15 @@ def check_previous_score():
             for row in rows[2:]:
                 print(" ".join(row))
         else:
-            print(f"\n{RED}No data found for '{data_name}'. Please check the name or take the test first.{RESET}")
+            print(f"\n{RED}No data found for '{data_name}'. Please check the spelling of your name or take the test first.{RESET}")
     else:
-        print(f"\n{RED}No data found for '{data_name}'. Please check the name or take the test first.{RESET}")
+        print(f"\n{RED}No data found for '{data_name}'. Please check the spelling of your name or take the test first..{RESET}")
 
 def info():
     """
     Information about the purpose of psychological testing and instructions for taking the test
     """
-    welcome_text = "\n Welcome to psychological testing. By passing the temperament test, you will be able to better know your own Self. You will understand what your character is like and will be able to take a more correct position in life. Knowing the temperament of your loved ones and friends will help you get along comfortably in the family and in the work team.\n"
+    welcome_text = f"\n{BLUE} Welcome to psychological testing.{RESET} By passing the temperament test, you will be able to better know your own Self. You will understand what your character is like and will be able to take a more correct position in life. Knowing the temperament of your loved ones and friends will help you get along comfortably in the family and in the work team.\n"
 
     for letter in welcome_text:
         print(letter, end='',flush=True)
@@ -142,8 +140,10 @@ def validate_data(values):
         return False
     
     print(f"\n{YELLOW}Instructions:{RESET}\n")
-    print(f"""  {GREEN}{values}{RESET}, you are asked to answer 57 questions. The questions are aimed at identifying your usual way of behavior. \
-    Try to imagine typical situations and give the first “natural” answer that comes to mind. If you agree with the statement, indicate 'Yes', if not, indicate 'No'.\n""")
+    print(f"  {GREEN}{values}{RESET}, you are asked to answer 57 questions. "
+    "The questions are aimed at identifying your usual way of behavior. "
+    "Try to imagine typical situations and give the first “natural” answer that comes to mind. "
+    f"If you agree with the statement, indicate {GREEN}'Yes'{RESET}, if not, indicate {GREEN}'No'{RESET}.\n")
      
     return True   
 
@@ -214,7 +214,7 @@ def determine_temperament(resalts_extra_intro, resalts_neuroticism):
     elif resalts_extra_intro > 12 and resalts_neuroticism > 12:
         temperament_type = 'Choleric'
     
-    print(f"\nYour predominant temperament type is {temperament_type}\n")
+    print(f"\nYour predominant temperament type is {BLUE}{temperament_type}{RESET}\n")
     return temperament_type
 
 def describe_temperament(resalts_extra_intro, temperament_type):
@@ -222,36 +222,28 @@ def describe_temperament(resalts_extra_intro, temperament_type):
     Describe the temperament type based on the result.
     """
     if temperament_type == 'Melancholic':
-        description_temperament_type = (
-            f"{GREEN}  Melancholic (weak, unbalanced) {RESET} - the owner of a slightly inhibited reaction. "
-            "Usually these are indecisive, closed people, prone to deep feelings. "
-            "They can easily and steadfastly solve life's problems. On the negative side, "
-            "a melancholic can be fearful, squeamish, concentrating on minor events and getting upset because of them.\n"
-        )
+        description_temperament_type = (f"{BLUE} Melancholic (weak, unbalanced) {RESET} - the owner of a slightly inhibited reaction. "
+        "Usually these are indecisive, closed people, prone to deep feelings. "
+        "They can easily and steadfastly solve life's problems. On the negative side, "
+        "a melancholic can be fearful, squeamish, concentrating on minor events and getting upset because of them.\n")
     elif temperament_type == 'Phlegmatic':
-        description_temperament_type = (
-            f"{GREEN}  Phlegmatic (strong, inert) {RESET} has a low level of activity. "
-            "He is calm, prudent, able to bring the work he has begun to the end. As a rule, "
-            "he treats his forces economically and does not waste them on unnecessary activities "
-            "or on those that he considers so. Negative manifestations: lethargy, apathy, "
-            "lack of will, weakly expressed emotional indicators. Others may seem boring and callous.\n"
-        )
+        description_temperament_type = (f"{BLUE} Phlegmatic (strong, inert) {RESET} has a low level of activity. "
+        "He is calm, prudent, able to bring the work he has begun to the end. As a rule, "
+        "he treats his forces economically and does not waste them on unnecessary activities "
+        "or on those that he considers so. Negative manifestations: lethargy, apathy, "
+        "lack of will, weakly expressed emotional indicators. Others may seem boring and callous.\n")
     elif temperament_type == 'Sanguine':
-        description_temperament_type = (
-            f"{GREEN}  Sanguine{RESET} - the person is sociable, cheerful, easily makes new acquaintances. "
-            "Such people are also called the soul of the company. His feelings are unstable, "
-            "and preferences often change. He is characterized by expressive gestures and facial expressions. "
-            "He constantly needs vivid impressions. In rare cases, he plans his day, spontaneity haunts the sanguine "
-            "throughout his life in almost all areas. According to the main properties of the central nervous system, "
-            "it has a strong and balanced character.\n"
-        )
+        description_temperament_type = (f"{BLUE} Sanguine{RESET} - the person is sociable, cheerful, easily makes new acquaintances. "
+        "Such people are also called the soul of the company. His feelings are unstable, "
+        "and preferences often change. He is characterized by expressive gestures and facial expressions. "
+        "He constantly needs vivid impressions. In rare cases, he plans his day, spontaneity haunts the sanguine "
+        "throughout his life in almost all areas. According to the main properties of the central nervous system, "
+        "it has a strong and balanced character.\n")
     elif temperament_type == 'Choleric':
-        description_temperament_type = (
-            f"{GREEN}  Choleric (an unbalanced, strong type of temperament){RESET} is energetic, "
-            "his actions are characterized by discontinuity. They can be harsh and emotional. "
-            "Due to excessive enthusiasm for any business, they act too diligently, as a result of which "
-            "they are quickly exhausted and tired. At its worst, the choleric becomes irritable and unable to control himself.\n"
-        )
+        description_temperament_type = (f"{BLUE} Choleric (an unbalanced, strong type of temperament){RESET} is energetic, "
+        "his actions are characterized by discontinuity. They can be harsh and emotional. "
+        "Due to excessive enthusiasm for any business, they act too diligently, as a result of which "
+        "they are quickly exhausted and tired. At its worst, the choleric becomes irritable and unable to control himself.\n")
     else:
         description_temperament_type = "Invalid temperament type"
     
@@ -260,24 +252,19 @@ def describe_temperament(resalts_extra_intro, temperament_type):
     return description_temperament_type
 
 
-
-
 def describe_introversion_extroversion(resalts_extra_intro):
     """
     Describe introversion or extroversion based on the result.
     """
     if resalts_extra_intro > 12:
-        description_extra_intro = (
-            "  The results also showed that you are an extroverted personality type. "
-            "This characterizes you as friendly, talkative, and energetic.\n"
-        )
+        description_extra_intro = (" The results also showed that you are an extroverted personality type. "
+        "This characterizes you as friendly, talkative, and energetic.\n")
+
     else:
-        description_extra_intro = (
-            "  The results also showed that you are an introverted personality type. "
-            "This manifests itself in more withdrawn and solitary behavior.\n"
-        )
+        description_extra_intro = (" The results also showed that you are an introverted personality type. "
+        "This manifests itself in more withdrawn and solitary behavior.\n")
     
-    # print(description_extra_intro)
+    print(description_extra_intro)
     return description_extra_intro
 
 
@@ -287,25 +274,19 @@ def describe_neuroticism(resalts_neuroticism):
     Describe the level of neuroticism based on the result.
     """
     if resalts_neuroticism <= 7:
-        description_resalts_neuroticism = (
-            "  You are usually characterized by stable and low-intensity emotional reactions. "
-            "You rarely experience extreme anxiety, nervousness, or depression and usually cope with stress better.\n"
-        )
+        description_resalts_neuroticism = (" You are usually characterized by stable and low-intensity emotional reactions. "
+        "You rarely experience extreme anxiety, nervousness, or depression and usually cope with stress better.\n")
+
     elif 8 < resalts_neuroticism <= 13:
-        description_resalts_neuroticism = (
-            "  You usually have more stable emotional reactions. You may experience stress and anxiety, "
-            "but not as much or as often as people with higher levels of neuroticism.\n"
-        )
+        description_resalts_neuroticism = (" You usually have more stable emotional reactions. You may experience stress and anxiety, "
+        "but not as much or as often as people with higher levels of neuroticism.\n")
+
     elif 14 < resalts_neuroticism <= 18:
-        description_resalts_neuroticism = (
-            "  You tend to have emotional fluctuations and reactions to stress. You may experience anxiety, "
-            "nervousness, and worry more often, but not as intensely as people with very high levels of neuroticism.\n"
-        )
+        description_resalts_neuroticism = (" You tend to have emotional fluctuations and reactions to stress. You may experience anxiety, "
+        "nervousness, and worry more often, but not as intensely as people with very high levels of neuroticism.\n")
     else:
-        description_resalts_neuroticism = (
-            "  You often experience intense and frequent emotional reactions. You may be prone to excessive anxiety, "
-            "fears, depression, and feelings of restlessness. You can easily fall into states of nervousness and uncertainty.\n"
-        )
+        description_resalts_neuroticism = (" You often experience intense and frequent emotional reactions. You may be prone to excessive anxiety, "
+        "fears, depression, and feelings of restlessness. You can easily fall into states of nervousness and uncertainty.\n")
     
 
     print(description_resalts_neuroticism)
@@ -318,10 +299,8 @@ def check_honesty(resalts_scale_lies):
     Check if the user's answers may not have been honest.
     """
     if resalts_scale_lies >= 5:
-        description_scale_lies = (
-            f"  {RED}Important! {data_name}, you answered not as you really are, "
-            f"but as you would like or as accepted in society. In other words, your answers are not reliable.{RESET}\n"
-        )
+        description_scale_lies = (f" {RED}Important!{GREEN} {data_name}{RESET}, you answered not as you really are, "
+        f"but as you would like or as accepted in society. In other words, your answers are not reliable.{RESET}\n")
         sign_profile = "The test subject was not sufficiently honest, the test results are not reliable."
     else:
         description_scale_lies = ""
@@ -349,7 +328,8 @@ def print_final_message(data_name):
     """
     Print a final message to the console.
     """
-    final_message = f"{GREEN}{data_name}, {RESET}{YELLOW}thanks for the answers, testing is completed!{RESET}"
+    final_message = (f"{GREEN}{data_name}, {RESET}{YELLOW}thanks for the answers, testing is completed!{RESET}\n"
+    "\n\nTo take the test again or view your results, select the desired item in the menu below: ")
     for letter in final_message:
         print(letter, end='', flush=True)
         time.sleep(0.005)
@@ -358,8 +338,5 @@ def print_final_message(data_name):
 
 if __name__ == "__main__":
     menu()
-    # resalts_extra_intro, resalts_neuroticism, resalts_scale_lies, temperament_type, description_extra_intro, description_resalts_neuroticism, sign_profile = psychological_test(get_questions())
-    description_temperament_type = describe_temperament(resalts_extra_intro, temperament_type)
-    # save_results_to_google_sheets(data_name, resalts_extra_intro, resalts_neuroticism, resalts_scale_lies, temperament_type, description_extra_intro, description_resalts_neuroticism, sign_profile, description_temperament_type)
     
      
